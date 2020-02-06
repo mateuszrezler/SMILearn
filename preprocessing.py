ba054f46ca86544c1d7a2d89c2951530456d1f24
Mateusz Rezler <mateuszrezler@gmail.com>
2020-02-06 12:46:46 +0100

    Refactoring of 'preprocessing.py' module

--- preprocessing.py
+++ preprocessing.py
@@ -1,26 +1,73 @@
 from numpy import array
+from re import sub
 
 
-def get_struct_features(component, max_branch=56, max_ring=48,
-                        bond_dict={
-                            '.': (0.5/3, 0, 0, 0, 0),
-                            '-': (1/3, 0, 0, 0, 0),
-                            '/': (1/3, 1, 0, 0, 0),
-                            '\\': (1/3, 0, 1, 0, 0),
-                            ':': (1.5/3, 0, 0, 0, 0),
-                            '=': (2/3, 0, 0, 0, 0),
-                            '#': (3/3, 0, 0, 0, 0),
-                            '(': (0, 0, 0, 1/2, 0),
-                            ')': (0, 0, 0, 2/2, 0)
-                        }):
+class FeatureCreator(object):
+    r"""
+    Creator of feature arrays.
 
-    if component[0] == ')' and len(component) > 1:
+    Methods
+    -------
+    get_chem_struct_features
+    """
+    def get_chem_struct_features(self, component, max_branch_len=1,
+                                 max_ring_int=9,
+                                 struct_features_dict={
+                                     '.': (0.5/3, 0, 0, 0, 0),
+                                     '-': (1/3, 0, 0, 0, 0),
+                                     '/': (1/3, 1, 0, 0, 0),
+                                     '\\': (1/3, 0, 1, 0, 0),
+                                     ':': (1.5/3, 0, 0, 0, 0),
+                                     '=': (2/3, 0, 0, 0, 0),
+                                     '#': (3/3, 0, 0, 0, 0),
+                                     '(': (0, 0, 0, 1/2, 0),
+                                     ')': (0, 0, 0, 2/2, 0)
+                                 }):
+        r"""
+        Translate symbols of structural elements into feature vectors.
 
-        return array((0, 0, 0, len(component)/max_branch, 0))
+        Parameters
+        ----------
+        component : str
+        max_branch_len : int, optional
+            Maximum permissible number of closing parentheses describing
+            branch length in DeepSMILES notation.
+        max_ring_int : int, optional
+            Maximum permissible number describing ring connection.
+        struct_features_dict : dict, optional
+            Dictionary with str keys and tuple values.
 
-    elif component[0].isdigit() or component[0] == '%':
+        Returns
+        -------
+        numpy.ndarray
+            Structure features (1D array).
 
-        return array((0, 0, 0, 0, int(re.sub('%', '', component))/max_ring))
+        Examples
+        --------
+        >>> fc = FeatureCreator()
+        >>> fc.get_chem_struct_features(':')
+        array([0.5, 0. , 0. , 0. , 0. ])
+        >>> fc.get_chem_struct_features('))', max_branch_len=10)
+        array([0. , 0. , 0. , 0.2, 0. ])
+        >>> fc.get_chem_struct_features('%20', max_ring_int=50)
+        array([0. , 0. , 0. , 0. , 0.4])
+        """
+        if component[0] == ')' and len(component) > 1:
 
-    return array((bond_dict[component]))
+            scaled_to_max = len(component)/max_branch_len
+
+            return array((0, 0, 0, scaled_to_max, 0))
+
+        elif component[0].isdigit() or component[0] == '%':
+
+            scaled_to_max = int(sub('%', '', component))/max_ring_int
+
+            return array((0, 0, 0, 0, scaled_to_max))
+
+        return array((struct_features_dict[component]))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
