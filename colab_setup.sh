6ee066cef6d6fa82171cde63ac221bc039956527
Mateusz Rezler <mateuszrezler@gmail.com>
2020-02-14 12:06:21 +0100

    Addition of 'export' command, removal of 'asp.py' file

--- colab_setup.sh
+++ colab_setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Install Miniconda
-
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -bfp /usr/local
 rm Miniconda3-latest-Linux-x86_64.sh
 
 # Install RDKit
 conda install -qyc conda-forge rdkit
+
+# Set environment variable
+export PYTHONPATH="/usr/local/lib/python3.7/site-packages"
 
 # Install DeepSMILES unofficial GitHub fork
 pip install git+git://github.com/mateuszrezler/deepsmiles@master
