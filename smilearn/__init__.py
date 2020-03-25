99d32bf19df4df67de9d879a857678a938a5e428
Mateusz Rezler <mateuszrezler@gmail.com>
2020-03-25 23:24:39 +0100

    Update of '__init__.py' file

---
+++ smilearn/__init__.py
@@ -0,0 +1,9 @@
+"""SMILearn - a package for creating feature matrices for deep learning models,
+based on SMILES notation of chemical compounds"""
+
+__name__ = 'smilearn'
+__author__ = 'Mateusz Rezler'
+__version__ = '0.1.0'
+__license__ = 'GNU General Public License v3.0'
+__copyright__ = 'Mateusz Rezler 2020'
+
